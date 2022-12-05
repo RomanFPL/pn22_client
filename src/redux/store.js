3eec1ad2fdcef14c0572995f4b3196885968f53f
IgorKrot <108532514+IgorKrot@users.noreply.github.com>
2022-12-05 17:38:45 +0200

    bags

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import categoryData from './reduxSlices/categorySlice'
 import userdata from './reduxSlices/userSlice'
 import commondata from './reduxSlices/commonSlice'
-import { useDispatch } from 'react-redux'
 
 
 export const store = configureStore({
